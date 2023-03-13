277263718bbc09280b648ad2fc58512dea0c9fa2
adrian <adrian@iraizos.me>
2023-03-13 19:24:56 +0100

    Initial Commit

---
+++ ScoresApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScoresApp
+//
+//  Created by Adrian Iraizos Mendoza on 13/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
